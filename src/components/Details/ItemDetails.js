c2ea0a5227969341ccb31ab14ad9be8828c4ffc8
Pasarica Diana <pasaricadiana@yahoo.com>
2021-09-23 23:54:10 +0300

    Submit bid form

--- src/components/Details/ItemDetails.js
+++ src/components/Details/ItemDetails.js
@@ -7,7 +7,7 @@ import ApiRequestService from '../../ApiHelpers/ApiRequestService';
 const ItemDetails = () => {
     const { productId } = useParams();
     const [product, setProduct] = useState([]);
-    const [user, setUser] = useState();
+    const [user, setUser] = useState([]);
     useEffect(() => {
         GetProduct();
 
@@ -23,7 +23,9 @@ const ItemDetails = () => {
         })
 
     }
-
+    const submitForm = () => {
+        console.log("data")
+    }
 
     return (
         <>
@@ -36,12 +38,25 @@ const ItemDetails = () => {
                     </div>
                     <div className="col-4 " >
                         <div className="containerBorder borderSize">
-                            <p>aaa</p>
+                            <p className="textStyle mt-xxl-4 m-xxl-3">Price:
+                                <span className="info"> ${product.price}</span>
+                            </p>
+                            <p className="textStyle m-xxl-3 mt-auto">Number of bids:
+                                <span className="info"> {product.bidNr}</span>
+                            </p>
+                            <form onSubmit={submitForm}>
+                                <div className="form-group">
+                                    <input type="text" className="form-control-sm m-xxl-3 size" placeholder="$0.00" name="value" />
+                                </div>
+                                <button type="submit" className="btn btn-bid">Submit a Bid</button>
+                                <span className="d-block text-center my-2">or</span>
+                                <button type="submit" className="btn  btn-pay m-auto">Buy Now</button>
+                            </form>
                         </div>
                         <div className="containerBorder borderSize">
                             <img src={user.imageUrl} className="userImage img-fluid rounded-circle" />
                             <p className="textName">{user.userName}</p>
-                            <p className="userCredentials">Owner</p>
+                            <p className="textStyle text-center">Owner</p>
                             <p className="text"> {user.description || `User didn't added description`}</p>
                         </div>
                     </div>
